49134d72ca2133542090c67d0bdc37d5c080d678
BaderSerhan <baderserhan@gmail.com>
2021-11-12 20:26:35 +0200

    rename project packages

---
+++ src/session7_panes/BorderPaneDemoWithPanes.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session7_panes;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author bader-aul
+ */
+public class BorderPaneDemoWithPanes extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        //initialize border pane
+        BorderPane root = new BorderPane();
+
+        //---------------------------------------------
+        //initialize top component using a stack pane
+        Label top = new Label("TOP");
+        StackPane topStack = new StackPane(top);
+        topStack.setStyle("-fx-border-color:green");
+
+        //add top stack to border pane as its top
+        root.setTop(topStack);
+
+        //---------------------------------------------
+        //initialize center component using ImageView
+        ImageView imageView = new ImageView("file:///Users/bader-aul/NetBeansProjects/AJ_Fall2022/lebanon.png");
+        //set exact height of imageView
+        imageView.setFitHeight(200);
+        //set preserve the aspect ratio of imageView, i.e. we can only set EITHER width OR height and the other one will be calculated based on the aspect ratio
+        imageView.setPreserveRatio(true);
+
+        //add imageView to border pane as its center
+        root.setCenter(imageView);
+
+        //---------------------------------------------
+        //initialize left component using VBox
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(new Label("Course"), new Label("Tutorial"), new Label("Lab"));
+        vbox.setSpacing(10);
+        vbox.setStyle("-fx-border-color:blue");
+        vbox.setAlignment(Pos.CENTER);
+
+        //add vbox to border pane as its left
+        root.setLeft(vbox);
+
+        //---------------------------------------------
+        //initialize bottom component using HBox
+        HBox hbox = new HBox();
+        hbox.getChildren().addAll(new Button("Computer Science"), new Button("CCE"));
+        hbox.setSpacing(10);
+        hbox.setStyle("-fx-border-color: purple");
+        hbox.setAlignment(Pos.CENTER);
+
+        //add hbox to border pane as its bottom
+        root.setBottom(hbox);
+
+        //---------------------------------------------
+        //Note: NO RIGHT COMPONENT IS SET
+        //SO NEARBY COMPONENTS WILL TAKE ITS SPACE
+        //---------------------------------------------
+        Scene scene = new Scene(root, 400, 400);
+
+        primaryStage.setTitle("BorderPane Demo");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ src/session8_shapes/ShowImage.java
@@ -0,0 +1,67 @@
+package session8_shapes;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class ShowImage extends Application {
+
+    //online image url prefixed with https://
+    String imageUrl = "https://picsum.photos/200";
+
+    @Override // Override the start method in the Application class
+    public void start(Stage primaryStage) {
+        // Create a pane to hold the image views
+        Pane pane = new HBox(10);
+        pane.setPadding(new Insets(5, 5, 5, 5));
+
+        /**
+         * create image from local path
+         * e.g. path:
+         * C:\\Users\\Bader\\Documents\\NetBeansProjects\\AJLab_Fall2021\\girl.png
+         *
+         * prefix it with 'file:///' or 'File:'
+         */
+        Image image = new Image("File:images/girl.png");
+
+        //first image view is created in-line using Image object
+        //no height or width is set => default image sizing is considered
+        pane.getChildren().add(new ImageView(image));
+
+        //second image view is created using online image URL
+        ImageView imageView2 = new ImageView(imageUrl);
+        //set fit height and width to image
+        imageView2.setFitHeight(100);
+        imageView2.setFitWidth(100);
+
+        pane.getChildren().add(imageView2);
+
+        //third image view created and rotated 90 degrees
+        ImageView imageView3 = new ImageView(image);
+        imageView3.setRotate(90);
+        //by setting only height OR width with preserve ratio, the original ratio of the image will be preserved
+        imageView3.setFitHeight(200);
+        imageView3.setPreserveRatio(true);
+
+        pane.getChildren().add(imageView3);
+
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(pane);
+        primaryStage.setTitle("ShowImage"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+    }
+
+    /**
+     * The main method is only needed for the IDE with limited
+     * JavaFX support. Not needed for running from the command line.
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
